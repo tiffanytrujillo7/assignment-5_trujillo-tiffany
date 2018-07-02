2d58e71bd68b8a8553c39980bcf5bb727007ce0e
Tiffany Trujillo <tiffanytrujillo@Tiffanys-MacBook-Air.local>
2018-07-02 15:43:42 -0400

    Added javascript for form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Event Listener Paragraph Clicks
+
 var heading = document.getElementById( 'mainHeader' );
 var paragraph = document.createElement('paragraph');
 var number = 1;
@@ -5,4 +7,24 @@ var number = 1;
 heading.addEventListener( "click", function() {
     paragraph.innerHTML = 'This is click number' + ' ' + number++;
     document.getElementsByTagName('body')[0].appendChild(paragraph);
+});
+
+//Event Listener form
+
+var submission = document.getElementById('form');
+
+submission.addEventListener('submit', function(submitting) {
+submitting.preventDefault();
+
+var firstName = document.getElementById('firstName').value;
+console.log('The firstName is:' + ' ' + firstName);
+
+var lastName = document.getElementById('lastName').value;
+console.log('The lastName is:' + ' ' + lastName);
+
+var email = document.getElementById('emailAddress').value;
+console.log('The email is:' + ' ' + email);
+
+var message = document.getElementById('message').value;
+console.log('The message is:' + ' ' + message);
 });
